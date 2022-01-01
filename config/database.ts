3b02bebc58b9c6175eb2177bcc67fcc4d73ca6aa
Devesh Mhatre <devesh@f22labs.com>
2022-01-01 23:18:43 +0530

    Keep SSL auth for DB in staging

--- config/database.ts
+++ config/database.ts
@@ -5,8 +5,8 @@
  * file.
  */
 
-import Env from '@ioc:Adonis/Core/Env';
-import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database';
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig = {
   /*
@@ -41,9 +41,6 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: {
-          rejectUnauthorized: false,
-        },
       },
       migrations: {
         naturalSort: true,
@@ -51,7 +48,8 @@ const databaseConfig: DatabaseConfig = {
       healthCheck: false,
       debug: false,
     },
-  },
-};
 
-export default databaseConfig;
+  }
+}
+
+export default databaseConfig
